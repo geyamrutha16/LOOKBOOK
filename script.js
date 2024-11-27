157061501da4b8c6baf70a3ea923d0f53ee00ca2
Geyamrutha Poluru <shinchanharry148@gmail.com>
2024-11-27 16:55:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Programmar", "MERN Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+// timeline.js
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".timeline-item");
+
+    const isInViewport = (elem) => {
+        const bounding = elem.getBoundingClientRect();
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const run = () =>
+        items.forEach((item) => {
+            if (isInViewport(item)) {
+                item.classList.add("show");
+            }
+        });
+
+    // Events
+    window.addEventListener("load", run);
+    window.addEventListener("resize", run);
+    window.addEventListener("scroll", run);
+});
+
+
